e19d5cffa0e8374b975417fc25a67c4159b62e5c
Kyryl Shekhanin <kyryl.shekhanin@outlook.com>
2024-05-14 20:35:17 +0300

    r_d8 tasks: 1, 2 (factorial coppied from prev ticket), 3, 4, 5

---
+++ r_d8/r_d8.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include "RD8.h"
+
+/*
+1) Функція приймає число - кількість чисел для вводу юзеру.Порахувати кількість введених юзером додатних чисел, від’ємних та 0. Вивести у консоль ці лічильники;
+
+Example input : 5    1 2 - 2 0 0
+
+Output : Positive : 2, negative : 1, zeroes : 2
+
+2) Функція int factorial(int count) приймає число - count!Написати функції, які обраховують значення факторіала :
+
+за допомогою цикла;
+з використанням рекурсії;
+3) Функція для друкування перших N натуральних чисел : void printNaturalNumbers(int count);
+
+від найбільшого до найменшого за допомогою цикла;
+від найменшого до найбільшого за допомогою цикла;
+від найбільшого до найменшого за допомогою рекурсії;
+від найменшого до найбільшого за допомогою рекурсії;
+4) Функції обрахунку площі та довжини кола : float getArea(float radius), float getCircumference(float radius);
+
+5) Винести декларації та дефінішини циклічних функцій в окрему пару header / cpp файл, а також рекурсивні функції в окрему пару header / cpp файл.
+*/
+
+int main()
+{
+    //countNumbers(3);
+    //factorialProgression();
+
+    //printNaturalNumbers(5, Ascending);
+    //printNaturalNumbers(5, Descending);
+    //printNaturalNumbers(5, Ascending, true);
+    //printNaturalNumbers(5, Descending, true);
+
+    getArea(1);
+    getCircumference(1);
+
+}
